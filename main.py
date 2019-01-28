f7aff6b545d3e2def728bff26215f9b5d9dcfc7a
Daniel Nielsen <daniel@norhoj.com>
2019-01-28 12:14:36 +0100

    General improvements including session management

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, jsonify
 from static import urls, auth
 from urllib.parse import quote
 import requests, api
 
 app = Flask(__name__)
 port = 8000
+app.config['SECRET_KEY'] = "FUCKINGLORT"
 
 @app.route('/')
 def index():
@@ -15,22 +16,28 @@ def index():
 
 	return render_template('index.html', url=url)
 
-@app.route('/response')
+@app.route('/callback')
 def response():
 	code = request.args.get('code')
 	
 	try:
 		res = api.exchange_code(code)
-	except requests.exceptions.HTTPError as e:
+	except requests.exceptions.HTTPError:
 		return "Invalid Code, try again."
 
+	session['token'] = res['access_token']
+	return redirect("/me")
+
+@app.route('/me')
+def me():
+	print(session['token'])
+
 	try:
-		user = api.get_info(res['access_token'])
-	except requests.exceptions.HTTPError as e:
-		print(e)
+		user = api.get_info(session['token'])
+	except requests.exceptions.HTTPError:
 		return "Invalid Token, try again."
 
-	return str(user)
+	return jsonify(user)
 	#return render_template('response.html')
 
 if __name__ == '__main__':
